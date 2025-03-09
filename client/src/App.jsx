7f0adb0f9ddffdcc5d2bc834e780c7dfef93cf3f
Gean2003 <i202114701@cibertec.edu.pe>
2025-03-09 18:09:56 -0500

    init tailwindcss and daisyui

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div data-theme="forest">
+      <h1 className='text-red-600   text-center font-bold text-3xl'>Init frontend</h1>
+    </div>
+  )
+}
+
+export default App
