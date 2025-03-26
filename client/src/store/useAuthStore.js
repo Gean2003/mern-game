9cf826a5d8c457db3c1d1002fd53aa2a17e5cf68
Gean2003 <i202114701@cibertec.edu.pe>
2025-03-26 15:44:29 -0500

    updated auth && room store

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 
 import { axiosInstance } from "../libs/axios";
+import toast from "react-hot-toast";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "/";
 
@@ -21,6 +22,34 @@ export const useAuthStore = create((set, get) => ({
             set({ authUser: null });
         }finally{
             set({ isCheckingAuth: false})
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        
+        try {
+            const res = await axiosInstance.post("/auth/signup", data)
+            set({ authUser: res.data })
+            toast.success("Account created successfully")
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }finally{
+            set({ isSigningUp: false})
+        }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }finally{
+            set({ isLoggingIn: false })
         }
     }
 
