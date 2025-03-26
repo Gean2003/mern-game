91ca149281aece91b776e314c796aeb6831a0710
Gean2003 <i202114701@cibertec.edu.pe>
2025-03-26 15:45:49 -0500

    modal add in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { LogOut, Settings, User, Gamepad2, UserRoundPlus } from "lucide-react";
+import { useState } from "react";
+import { AddFriendModal } from "./AddFriendModal";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
+  const [isModal, setIsModal] = useState(false);
+
+  const toggleModal = () => {
+    setIsModal(!isModal)
+  }
+
+  if (isModal){
+    return (
+      <div className='w-full h-full absolute  bg-black sm:opacity-80 z-10 '>
+      
+      <AddFriendModal toggleModal={toggleModal} />
+
+      </div>
+        
+    )
+  }
 
   return (
     <header
@@ -15,9 +33,9 @@ const Navbar = () => {
           <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-primary" />
+                <Gamepad2 className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <h1 className="text-lg font-bold">Rock, Paper && scissors</h1>
             </Link>
           </div>
 
@@ -35,6 +53,11 @@ const Navbar = () => {
 
             {authUser && (
               <>
+                <button className={`btn btn-sm gap-2`} onClick={toggleModal}>
+                  <UserRoundPlus className="size-5" />
+                  <span className="hidden sm:inline">Add friend</span>
+                </button>
+
                 <Link to={"/profile"} className={`btn btn-sm gap-2`}>
                   <User className="size-5" />
                   <span className="hidden sm:inline">Profile</span>
@@ -44,6 +67,7 @@ const Navbar = () => {
                   <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
+
               </>
             )}
           </div>
